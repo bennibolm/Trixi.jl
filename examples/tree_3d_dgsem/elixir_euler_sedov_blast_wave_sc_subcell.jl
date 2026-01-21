921335f924ff20d81204f0f145a3a657a18b439f
Benjamin Bolm <74359358+bennibolm@users.noreply.github.com>
2026-01-21 09:47:08 +0100

    Update examples/tree_3d_dgsem/elixir_euler_sedov_blast_wave_sc_subcell.jl

--- examples/tree_3d_dgsem/elixir_euler_sedov_blast_wave_sc_subcell.jl
+++ examples/tree_3d_dgsem/elixir_euler_sedov_blast_wave_sc_subcell.jl
@@ -8,8 +8,8 @@ equations = CompressibleEulerEquations3D(1.4)
 """
     initial_condition_sedov_blast_wave(x, t, equations::CompressibleEulerEquations3D)
 
-The Sedov blast wave setup based on Flash
-- https://flash.rochester.edu/site/flashcode/user_support/flash_ug_devel/node187.html#SECTION010114000000000000000
+The Sedov blast wave setup based on example 35.1.4 from Flash
+- https://flash.rochester.edu/site/flashcode/user_support/flash4_ug_4p8.pdf
 with smaller strength of the initial discontinuity.
 """
 function initial_condition_sedov_blast_wave(x, t,
