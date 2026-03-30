de9d37b411d13d0315db4c8ad5ba6df5ed28a609
bennibolm <benjamin.bolm@gmx.de>
2026-03-30 14:25:23 +0200

    Fix

--- src/solvers/dgsem_tree/dg_2d.jl
+++ src/solvers/dgsem_tree/dg_2d.jl
@@ -180,11 +180,11 @@ function rhs!(du, u, t,
     return nothing
 end
 
-function calc_volume_integral!(du, u, mesh,
+function calc_volume_integral!(backend::Nothing, du, u, mesh,
                                nonconservative_terms, equations,
                                volume_integral::AbstractVolumeIntegral,
                                dg, cache, t, boundary_conditions)
-    calc_volume_integral!(du, u, mesh,
+    calc_volume_integral!(backend, du, u, mesh,
                           nonconservative_terms, equations,
                           volume_integral, dg, cache)
 
