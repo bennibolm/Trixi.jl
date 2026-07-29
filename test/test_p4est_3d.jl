7b23cdef88a8199443bd087847db49cdf0529d62
bennibolm <benjamin.bolm@gmx.de>
2026-07-29 13:42:25 +0200

    Small change

--- test/test_p4est_3d.jl
+++ test/test_p4est_3d.jl
@@ -647,7 +647,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 15_000)
 end
 
-@testitem "P4estMesh3D Euler: elixir_euler_mortar_sc_subcell.jl (local limiting with bar states)" setup=[
+@testitem "P4estMesh3D: elixir_euler_mortar_sc_subcell.jl (local limiting with bar states)" setup=[
     Setup,
     P4estMesh3D
 ] tags=[:p4est_part2] begin
