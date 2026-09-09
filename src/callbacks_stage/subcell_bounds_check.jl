6188101480c9d039a6c579e9b6e6829c912e7d6c
bennibolm <benjamin.bolm@gmx.de>
2026-09-09 14:32:17 +0200

    Adaptations in bounds check routines

--- src/callbacks_stage/subcell_bounds_check.jl
+++ src/callbacks_stage/subcell_bounds_check.jl
@@ -36,9 +36,8 @@ end
 function (callback::BoundsCheckCallback)(u_ode, integrator, stage)
     mesh, equations, solver, cache = mesh_equations_solver_cache(integrator.p)
 
-    if ndims(equations) == 2 &&
-       solver.volume_integral isa VolumeIntegralSubcellLimiting &&
-       solver.volume_integral.limiter.indicator !== nothing
+    if solver.volume_integral isa VolumeIntegralSubcellLimiting &&
+       !isnothing(solver.volume_integral.limiter.indicator)
         # When using a smoothness indicator, a convex combination of the limiting factors from
         # local and positivity limiting are used. However, the deviations are computed solely with
         # respect to the local bounds. Consequently, the resulting deviation statistics would not
@@ -168,17 +167,16 @@ end
         println("Note: The following deviations are only computed in elements where subcell limiting is active.")
         println("In other elements, the solution is not checked for bounds violations.")
     end
-    if (ndims(semi.equations) == 2 || ndims(semi.equations) == 3) &&
-       semi.solver.volume_integral isa VolumeIntegralSubcellLimiting &&
+    if !idp_newton_converged[]
+        println("Note: Newton-bisection method reached the maximum number of iterations at least once.")
+    end
+    if semi.solver.volume_integral isa VolumeIntegralSubcellLimiting &&
        !isnothing(limiter.indicator)
         println("Due to the use of a smoothness indicator, a convex combination of the limiting factors of local and")
         println("positivity limiting was employed. However, the deviations are computed solely with respect to the local")
         println("bounds. Consequently, the resulting deviation statistics may not be meaningful and can exceed zero.")
         println("─"^100 * "\n")
         return nothing
-    end
-    if !idp_newton_converged[]
-        println("Note: Newton-bisection method reached the maximum number of iterations at least once.")
     end
     if local_twosided
         for v in limiter.local_twosided_variables_cons
